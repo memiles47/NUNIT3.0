7f522cc640486b8f6242164bc9c103096bc314d1
Michael E Miles <michael@miles-me.net>
2016-11-13 10:59:44 -0600

    Setting up the first test. Test using Resharper

---
+++ Chapter3_GettingStarted/Chapter3_GettingStarted/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Chapter3_GettingStarted
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void MyFirstTestMethod()
+        {
+            //Test code here
+            Console.WriteLine("Inside MyFirstTestMethod");
+        }
+    }
+}
