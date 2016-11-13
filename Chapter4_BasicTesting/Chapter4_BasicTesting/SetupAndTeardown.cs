e615b898da4fe551857f4488678cc42f8fbba449
Michael E Miles <michael@miles-me.net>
2016-11-13 11:35:38 -0600

    Created the basic structure for unit tests

---
+++ Chapter4_BasicTesting/Chapter4_BasicTesting/SetupAndTeardown.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Chapter4_BasicTesting
+{
+    [TestFixture]
+    public class SetupAndTeardown
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            Console.WriteLine("This runs once before all of the tests in this test fixture");
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            Console.WriteLine("This runs once after all of the testx in this test fixgure");
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("This runs before each test in this test fixture");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Console.WriteLine("This runs after each test in this test fixture");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine("This is inside Test 1");
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Console.WriteLine("This is inside Test 2");
+        }
+    }
+}
---
+++ Chapter4_BasicTesting/Chapter4_BasicTestingTests/SetupAndTeardownTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Chapter4_BasicTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4_BasicTesting.Tests
+{
+    [TestClass()]
+    public class SetupAndTeardownTests
+    {
+        [TestMethod()]
+        public void OneTimeSetUpTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void OneTimeTearDownTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetupTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TearDownTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Test1Test()
+        {
+            Assert.Fail();
+        }
+
+        public void Test2Test()
+        {
+
+        }
+    }
+}
