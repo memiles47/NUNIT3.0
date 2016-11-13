4c249a3dd004210b683961351ee8e9f2ebd3131c
Michael E Miles <michael@miles-me.net>
2016-11-13 16:27:56 -0600

    Refactor to represent the books way of doing unit testing

---
+++ Chapter4_BasicTesting/Chapter4_BasicTesting/GivenWeAlreadyHaveACalculatorObject.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Chapter4_BasicTesting
+{
+    [TestFixture]
+    internal class GivenWeAlreadyHaveACalculatorObject
+    {
+        private Calculator Calculator { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            Calculator = new Calculator();
+        }
+
+        [Test]
+        public void AddingTwoNumbersShouldReturncorrectSum()
+        {
+            // When I add 3 and 7 together
+            var returnValue = Calculator.add(3, 7);
+
+            // Then the result should be 10
+            Assert.That(returnValue, Is.EqualTo(10));
+        }
+    }
+}
