121db5f8a29fa99f040c6087a064d480c888b1fb
Michael E Miles <michael@miles-me.net>
2016-11-14 03:04:06 -0600

    The error was a Calculator was never instantiated

--- Chapter4_BasicTesting/Chapter4_BasicTesting/GivenWeHaveACalculatorObject.cs
+++ Chapter4_BasicTesting/Chapter4_BasicTesting/GivenWeHaveACalculatorObject.cs
@@ -5,14 +5,16 @@ namespace Chapter4_BasicTesting
     [TestFixture]
     internal class GivenWeHaveACalculatorObject
     {
-        private Calculator SUT { get; set; }
+        //private Calculator SUT { get; set; }
+
+        private readonly Calculator _sut = new Calculator();
 
         [Test]
         [TestCase(3, 7, ExpectedResult = 10)]
         public int AddingTwoNumbersShouldReturncorrectSum(int a, int b)
         {
             // When I add 3 and 7 together
-            var returnValue = SUT.Add(a, b);
+            var returnValue = _sut.Add(a, b);
 
             return returnValue;
         }
