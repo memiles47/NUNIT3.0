21f36ffec72aba216a2001eccceac6b91b343d12
Michael E Miles <michael@miles-me.net>
2016-11-16 07:32:46 -0600

    Update

--- Chapter6_ManagingYourTests/Chapter6_ManagingYourTests/OrderingTests.cs
+++ Chapter6_ManagingYourTests/Chapter6_ManagingYourTests/OrderingTests.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Chapter6_ManagingYourTests
+﻿namespace Chapter6_ManagingYourTests
 {
     public class OrderingTests
     {
