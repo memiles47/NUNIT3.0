2cf39954b782f46b35afadccb331e940a6ed0a26
Michael E Miles <michael@miles-me.net>
2016-11-18 10:14:42 -0600

    Testing flow control of tests - Not something that should be done.

--- Chapter6_ManagingYourTests/Chapter6_ManagingYourTests/OrderingTests.cs
+++ Chapter6_ManagingYourTests/Chapter6_ManagingYourTests/OrderingTests.cs
@@ -1,6 +1,37 @@
-﻿namespace Chapter6_ManagingYourTests
+﻿using System;
+using NUnit.Framework;
+
+namespace Chapter6_ManagingYourTests
 {
+    [TestFixture]
+
     public class OrderingTests
     {
+        public static IEquatable<TestCaseData> MainTests
+        {
+            get
+            {
+                yield return new TestCaseData(new Action(() =>
+                    {
+                        System.Threading.Thread.Sleep(5000);
+                        Console.WriteLine("This is output 1");
+                    }
+                ));
+
+                yield return new TestCaseData(new Action(() =>
+                {
+                    System.Threading.Thread.Sleep(5000);
+                    Console.WriteLine("This is output 2");
+                }
+                ));
+
+                yield return new TestCaseData(new Action(() =>
+                {
+                    System.Threading.Thread.Sleep(5000);
+                    Console.WriteLine("This is output 3");
+                }
+                ));
+            }
+        }
     }
 }
