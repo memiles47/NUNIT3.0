5177b22790fc84b2b6698882d012eb5a52d4ecac
Michael E Miles <michael@miles-me.net>
2017-01-12 03:13:19 -0600

    Testing

--- Chapter6_ManagingYourTests/Chapter6_ManagingYourTests/OrderingTests.cs
+++ Chapter6_ManagingYourTests/Chapter6_ManagingYourTests/OrderingTests.cs
@@ -10,9 +10,7 @@ namespace Chapter6_ManagingYourTests
     {
         public static IEnumerable<TestCaseData> MainTests
         {
-            //testing Day 15 the first day of the first week of work for the new year
-            //My Peloton arrived last night much later than was expected but this
-            //machine is built well!
+            //testing Day 24 
             get
             {
                 yield return new TestCaseData(new Action(() =>
