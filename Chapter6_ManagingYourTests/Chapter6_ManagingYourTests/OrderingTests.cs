59d7d7901bab264309a2a7d62122bba4aa6b4e39
Michael E Miles <michael@miles-me.net>
2017-02-10 03:13:30 -0600

    Happy Birthday Angie

--- Chapter6_ManagingYourTests/Chapter6_ManagingYourTests/OrderingTests.cs
+++ Chapter6_ManagingYourTests/Chapter6_ManagingYourTests/OrderingTests.cs
@@ -12,8 +12,7 @@ namespace Chapter6_ManagingYourTests
         {
             //I am truly glad Angie and I are talking again
             //but I have trust issues....
-            //Siva let me know he will tell me where he is going
-            //next week.
+            //Happy Birthday Angie
             get
             {
                 yield return new TestCaseData(new Action(() =>
